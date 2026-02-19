df97574e8c246a2f3545452697aa16fc9dfd7f7c
Justin Johnson <ijustynclark@gmail.com>
2026-02-19 11:25:45 -0800

    zsh: make zshrc portable across users; harden compinit; avoid hardcoded paths

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -10,11 +10,6 @@ export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export EDITOR="nvim"
 export VISUAL="nvim"
 
-# Homebrew
-if [[ -f /opt/homebrew/bin/brew ]]; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-fi
-
 # Go
 export GOPATH="$HOME/go"
 export PATH="$GOPATH/bin:$PATH"
